a31abac34bc3ae0ae44df9cde2a1976b9fc0d489
Castro-Wetzstein Francisco <20073@columbushs.com>
2019-08-30 09:37:59 -0400

    FirstCommit

---
+++ src/pkgclass/schedule/ClassSchedule.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.schedule;
+
+/**
+ *
+ * @author 20073
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String name = "Francisco Casto";
+         int graduateYear = 2020;
+         System.out.println ("My name is (name)and I'll graduate in (graduateYear)");
+         
+    }
+}
+
